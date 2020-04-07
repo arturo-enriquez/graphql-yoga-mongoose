b911d921da9e2087497a2635fe24e9a6f99eb95b
Arturo Enriquez <arturoccompany@gmail.com>
2020-04-06 18:54:30 -0700

    Mutation createUser

--- src/resolvers.js
+++ src/resolvers.js
@@ -5,5 +5,20 @@ export const resolvers = {
     async getUsers() {
       return await User.find()
     }
+  },
+
+  Mutation: {
+    async createUser(_, args) {
+      const { nickname, fullname, phone, city } = args;
+
+      const user = new User({
+        nickname,
+        fullname,
+        phone,
+        city
+      });
+
+      return await user.save();
+    }
   }
 }
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -3,6 +3,15 @@ export const typeDefs = `
     getUsers: [User]!
   }
 
+  type Mutation {
+    createUser(
+      nickname: String!, 
+      fullname: String!,
+      phone: String,
+      city: String
+    ): User!
+  }
+
   type User {
     _id: ID!
     nickname: String!
